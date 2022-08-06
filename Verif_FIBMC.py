5529abc58b3a8714d1f2e54e3e65a4ed61124dad
00332275 <00332275@ufrgs.br>
2022-08-06 20:55:59 -0300

    v06/08

--- Verif_FIBMC.py
+++ Verif_FIBMC.py
@@ -85,11 +85,11 @@ for i in range(1001):
 eta = epsc/epsc1
 sigmac_i = fcm*(k*eta - eta**2)/(1+(k-2)*eta)
 ajust4 = np.poly1d(np.polyfit(epsc, sigmac_i, 4))
-a0 = ajust4[0]
-a1 = ajust4[1]
-a2 = ajust4[2]
-a3 = ajust4[3]
-a4 = ajust4[4]
+a0 = ajust4[0]/fcm
+a1 = ajust4[1]/fcm
+a2 = ajust4[2]/fcm
+a3 = ajust4[3]/fcm
+a4 = ajust4[4]/fcm
 
 # plot gráficos
 sigmac4 = ajust4(epsc)
@@ -166,7 +166,8 @@ Variáveis:
     rj() - porcentagem de armaduras das barras
     Es - módulo de def. longitudinal do aço
 """
-def esfor(As, a1, a2, Es, epscu, epsc2, fcd, fyk, gamas, Ly, rj, X, xc, xg, yc, yg, ys):
+def esfor(As, a1, a2, a3, a4, Es, epscu, epsc2, fcm, fyk, gamas,
+          Ly, rj, X, xc, xg, yc, yg, ys):
     YS = np.max(yc)
     YI = np.min(ys)
     d  = YS-YI
@@ -187,15 +188,15 @@ def esfor(As, a1, a2, Es, epscu, epsc2, fcd, fyk, gamas, Ly, rj, X, xc, xg, yc, 
     b = (epsS-epsI)/(YS-YI)
     c = epsS - b*YS
 
-    xx1, yy1, xx2, yy2 = poli(xc, yc, c, b, epsc2)
-    Mrx1, Nr1 = reg1(a1, a2, c, b, fcd, xx1, yy1)
-    Mrx2, Nr2 = reg2(fcd, xx2, yy2)
+    xx1, yy1 = poli(xc, yc, c, b)
+    Mrx1, Nr1 = reg1(a1, a2, a3, a4, c, b, fcm, xx1, yy1)
+    #Mrx2, Nr2 = reg2(fcd, xx2, yy2)
     
     fyd = fyk/gamas
     Mrxas, Nras = aco(As, b, c, Es, fyd, rj, ys)
     
-    Mrxd = Mrx1 + Mrx2 + Mrxas
-    Nrd = Nr1 + Nr2 + Nras
+    Mrxd = Mrx1 + Mrxas
+    Nrd = Nr1 + Nras
     
     return (Mrxd, Nrd, epsS, epsI)
 #############################
@@ -216,10 +217,8 @@ Variáveis:
 Last-Modified: 17/07/2022
 Status: Ok - Testado para seções retangulares e T.
 """
-def poli(xc, yc, c, b, epsc2):
+def poli(xc, yc, c, b):
     y01 = (-c/b)
-    y12 = (-epsc2-c)/b
-
     xx1 = np.copy(xc)
     yy1 = np.copy(yc)
 
@@ -237,42 +236,9 @@ def poli(xc, yc, c, b, epsc2):
         else:
             continue
     
-    for i in range(np.size(yy1)):
-        if i<(np.size(yy1)-1):
-            if yy1[i]<y12 and yy1[i+1]>y12:
-                yy1[i+1] = y12
-                xx1[i+1] = (y12-yc[i])/(yc[i+1]-yc[i])*(xc[i+1]-xc[i])+xc[i]
-            elif yy1[i]>y12 and yy1[i+1]<y12:
-                yy1[i] = y12
-                xx1[i] = (y12-yc[i+1])/(yc[i]-yc[i+1])*(xc[i]-xc[i+1])+xc[i+1]
-            elif yy1[i]>y12:
-                yy1[i] = np.nan
-                xx1[i] = np.nan
-            else:
-                continue
     yy1=yy1[np.logical_not(np.isnan(yy1))]
     xx1=xx1[np.logical_not(np.isnan(xx1))]
-
-    xx2 = np.copy(xc)
-    yy2 = np.copy(yc)
-
-    for i in range(np.size(yy2)):
-        if yy2[i]<y12:
-            yy2[i] = np.nan
-            xx2[i] = np.nan
-        elif np.isnan(yy2[i-1])==True:
-            yy2[i-1] = y12
-            xx2[i-1] = (y12-yc[i-1])/(yc[i]-yc[i-1])*(xc[i]-xc[i-1])+xc[i-1]
-        elif i<(np.size(yy2)-1):
-            if (yy2[i+1]<y12) and (yy2[i]>y12):
-                yy2[i+1] = y12
-                xx2[i+1] = (y12-yc[i])/(yc[i+1]-yc[i])*(xc[i+1]-xc[i])+xc[i]
-        else:
-            continue
-    yy2=yy2[np.logical_not(np.isnan(yy2))]
-    xx2=xx2[np.logical_not(np.isnan(xx2))]
-    
-    return (xx1, yy1, xx2, yy2)
+    return (xx1, yy1)
 #############################
 
 #############################
@@ -285,11 +251,13 @@ Variáveis:
     Mrx1, Nr1 - esforços resistentes da reg. 1
     G00, G01, G02, G03 - polinômios de integração
 """
-def reg1(a1, a2, c, b, fcd, xx1, yy1):
-    D0   = a1*c + a2*c**2
-    D1   = a1*b + 2*a2*b*c
-    D2   = a2*b**2
-    scd  = 0.85*fcd
+def reg1(a1, a2, a3, a4, c, b, fcm, xx1, yy1):
+    D0   = a1*c + a2*c**2 + a3*c**3 + a4*c**4
+    D1   = a1*b + 2*a2*b*c + 3*a3*b*c**2 + 4*a4*b*c**3
+    D2   = a2*b**2 + 3*a3*b**2*c + 6*a4*b**2*c**2
+    D3   = a3*b**3 + 4*a4*b**3*c
+    D4   = a4*b**4 
+    scd  = fcm
     Mrx1 = 0
     Nr1  = 0
     for i in range(np.size(xx1)-1):
@@ -305,13 +273,22 @@ def reg1(a1, a2, c, b, fcd, xx1, yy1):
         else:
             dy2  = dy*dy
             dy3  = dy2*dy
+            dy4  = dy3*dy
+            dy5  = dy4*dy
+            dy6  = dy5*dy
             G00  = (x1 + dx/2)*dy
             G01  = (x1*(y1+dy1)+dx*(y1/2+dy/3))*dy
             G02  = (x1*(y1*(dy+y1)+dy2/3) + dx*(y1*(y1/2+dy/1.5)+dy2/4))*dy
             G03  = (x1*(y1*(dy2+y1*(1.5*dy+y1))+dy3/4) + 
                     dx*(y1*(0.75*dy2+y1*(dy+y1/2))+dy3/5))*dy
-            Mrx1 = Mrx1 + scd*(D0*G01 + D1*G02 + D2*G03)
-            Nr1  = Nr1 + scd*(D0*G00 + D1*G01 + D2*G02)
+            G04  = (x1*y1**4*dy + dx*dy5/6 + dy4*(x1*dy+4*y1*dx)/5 +
+                    dy3*(2*x1*y1*dy + 3*y1**2*dx)/2 + dy2*(6*x1*y1**2*dy + 4*y1**3*dx)/3 +
+                    dy*(4*x1*y1**3*dy + y1**4*dx)/2)
+            G05  = (x1*y1**5*dy + dx*dy6/7 + dy5*(x1*dy + 5*y1*dx)/6 +
+                    dy4*(x1*y1*dy + 2*y1**2*dx) + dy3*(5*x1*y1**2*dy + 5*y1**3*dx)/2 +
+                    dy2*(10*x1*y1**3*dy + 5*y1**4*dx)/3 + dy*(5*x1*y1**4*dy + y1**5*dx)/2)
+            Mrx1 = Mrx1 + scd*(D0*G01 + D1*G02 + D2*G03 + D3*G04 + D4*G05)
+            Nr1  = Nr1 + scd*(D0*G00 + D1*G01 + D2*G02 + D3*G03 + D4*G04)
     return (Mrx1, Nr1)
 #############################
 #############################
@@ -385,30 +362,32 @@ def nlsistema(var, *var_aux):
     As    = const[0]
     a1    = const[1]
     a2    = const[2]
-    Es    = const[3]
-    fcd   = const[4]
-    fyk   = const[5]
-    gamas = const[6]
-    Ly    = const[7]
-    Maxd  = const[8]
-    Nad   = const[9]
-    Mrxd, Nrd, epsS, epsI = esfor(As, a1, a2, Es, epscu, epsc2, fcd, fyk,
+    a3    = const[3]
+    a4    = const[4]
+    Es    = const[5]
+    fcm   = const[6]
+    fyk   = const[7]
+    gamas = const[8]
+    Ly    = const[9]
+    Maxd  = const[10]
+    Nad   = const[11]
+    Mrxd, Nrd, epsS, epsI = esfor(As, a1, a2, a3, a4, Es, epscu, epsc2, fcm, fyk,
                                   gamas, Ly, rj, X, xc, xg, yc, yg, ys)
     f = lamb*(Mrxd) - Maxd
     g = lamb*(Nrd) - Nad
     return[f,g]
-"""
-const = np.array([As, a1, a2, Es, fcd, fyk, gamas, Ly, Maxd, Nad])
+
+const = np.array([As, a1, a2, a3, a4, Es, fcm, fyk, gamas, Ly, Maxd, Nad])
 lamb_i = 1 #lambda inicial
 X_i = 0.5*Ly #altura da LN inicial
 s0 = np.array([X_i, lamb_i])
-var_aux = (const, epscu, epsc2, rj, xc, xg, yc, yg, ys)
-#X, lamb  = fsolve(nlsistema, s0, var_aux)
+var_aux = (const, epsc_lim, epsc1, rj, xc, xg, yc, yg, ys)
+X, lamb  = fsolve(nlsistema, s0, var_aux)
 #Mrxd, Nrd, epsS, epsI = esfor(As, a1, a2, Es, epscu, epsc2, fcd, fyk,
 #                              gamas, Ly, rj, X, xc, xg, yc, yg, ys)
 #FS = 1/lamb
 
-
+"""
 # Exportar resultados para Excel
 out = xlsxwriter.Workbook('saída.xlsx')
 worksheet = out.add_worksheet()
